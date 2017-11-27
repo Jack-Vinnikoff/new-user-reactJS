12d73408e6e3a177e9fb7f222f702ad6b37660ad
Demon <vinnicox@gmail.com>
2017-11-28 01:45:38 +0400

    created new component added-new-user-btn

---
+++ src/components/added-new-user-btn.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class BtnNewUser extends React.Component {
+    render() {
+        return (
+            <div className="field-btn-create">
+                <button type="submit">Добавить</button>
+            </div>
+        )
+    }
+}
+
+export default BtnNewUser;
--- src/container.jsx
+++ src/container.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import NewUser from './components/new-user.jsx'
+import BtnNewUser from './components/added-new-user-btn.jsx'
 
 class LogicApp extends React.Component {
     render(){
         return (
                 <div className="field">
                     <NewUser />
+                    <BtnNewUser />
                 </div>
         )
     }

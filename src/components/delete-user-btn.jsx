0abd46a1b29c3bed0aed636d5e9afaf1216e0e6a
Demon <vinnicox@gmail.com>
2017-11-30 03:29:59 +0400

    fix component delete-user-btn

--- src/components/delete-user-btn.jsx
+++ src/components/delete-user-btn.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
-class DeleteBtn extends React.Component {
-    render() {
-        return (
-            <button className="delete-btn">Delete</button>
-        )
-    }
+export default function DeleteBtn () {
+    return (
+        <button className="delete-btn">Delete</button>
+    )
 }
-
-export default DeleteBtn;
+// class DeleteBtn extends React.Component {
+//     render() {
+//         return (
+//             <button className="delete-btn">Delete</button>
+//         )
+//     }
+// }
+//
+// export default DeleteBtn;

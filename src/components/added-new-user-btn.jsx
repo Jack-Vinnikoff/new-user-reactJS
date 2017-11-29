678e46f9b3fa060b90d91ad7fdbe786cb24042e9
Demon <vinnicox@gmail.com>
2017-11-30 03:31:54 +0400

    fix component added-new-user-btn

--- src/components/added-new-user-btn.jsx
+++ src/components/added-new-user-btn.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
-class BtnNewUser extends React.Component {
-    render() {
-        return (
-            <div className="field-btn-create">
-                <button type="submit" onClick={this.props.a}>Добавить</button>
-            </div>
-        )
-    }
+export default function BtnNewUser () {
+    return (
+        <div className="field-btn-create">
+            <button type="submit">Добавить</button>
+        </div>
+    )
 }
-
-export default BtnNewUser;
+// class BtnNewUser extends React.Component {
+//     render() {
+//         return (
+//             <div className="field-btn-create">
+//                 <button type="submit" onClick={this.props.a}>Добавить</button>
+//             </div>
+//         )
+//     }
+// }
+//
+// export default BtnNewUser;

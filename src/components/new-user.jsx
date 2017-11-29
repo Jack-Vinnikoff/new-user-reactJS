f09174767fdb4da54c94a2190b42ad98755eb821
Demon <vinnicox@gmail.com>
2017-11-30 03:26:38 +0400

    fix component new-user

--- src/components/new-user.jsx
+++ src/components/new-user.jsx
@@ -1,25 +1,45 @@
 import React from 'react';
 
-class newUser extends React.Component {
-    render(){
-        return(
-            <div className="field-new-user">
-                <div className="field-new-user-top">
-                    <p>Добавить нового пользователя</p>
-                </div>
-                <div className="field-new-user-form">
-                    <form>
-                        <label htmlFor="name">Name</label>
-                        <input type="text" id="name" value="" placeholder="Name"/>
-                        <label htmlFor="age">Age</label>
-                        <input type="text" id="age" value="" placeholder="Age"/>
-                        <label htmlFor="note">Note</label>
-                        <input type="text" id="note" value="" placeholder="Note"/>
-                    </form>
-                </div>
-            </div>
-        )
-    }
+export default function newUser (){
+    return(
+             <div className="field-new-user">
+                 <div className="field-new-user-top">
+                     <p>Добавить нового пользователя</p>
+                 </div>
+                 <div className="field-new-user-form">
+                     <form>
+                         <label htmlFor="name">Name</label>
+                         <input type="text" id="name" value="" placeholder="Name"/>
+                         <label htmlFor="age">Age</label>
+                         <input type="text" id="age" value="" placeholder="Age"/>
+                         <label htmlFor="note">Note</label>
+                         <input type="text" id="note" value="" placeholder="Note"/>
+                     </form>
+                 </div>
+             </div>
+    )
 }
 
-export default newUser;
+// class newUser extends React.Component {
+//     render(){
+//         return(
+//             <div className="field-new-user">
+//                 <div className="field-new-user-top">
+//                     <p>Добавить нового пользователя</p>
+//                 </div>
+//                 <div className="field-new-user-form">
+//                     <form>
+//                         <label htmlFor="name">Name</label>
+//                         <input type="text" id="name" value="" placeholder="Name"/>
+//                         <label htmlFor="age">Age</label>
+//                         <input type="text" id="age" value="" placeholder="Age"/>
+//                         <label htmlFor="note">Note</label>
+//                         <input type="text" id="note" value="" placeholder="Note"/>
+//                     </form>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+//
+// export default newUser;

04323487e48346914ec7c834c490ea94a3c6f9e6
Demon <vinnicox@gmail.com>
2017-11-30 03:21:36 +0400

    fix component users-table and fix component show-users

--- src/components/users-table.jsx
+++ src/components/users-table.jsx
@@ -1,37 +1,67 @@
 import React from 'react';
 import ShowUsers from './show-users.jsx';
 
-class UsersTable extends React.Component {
-    render(){
-        return (
-            <div className="field-users-table">
-                <div className="field-users-table-head">
-                    <p>Users</p>
-                </div>
-                <table width="100%">
-                    <thead>
-                        <tr>
-                            <th>Users</th>
-                            <th>Email</th>
-                            <th>Address</th>
-                            <th>Delete</th>
-                            <th>Edit</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    {this.props.users.map((item) =>
-                        <ShowUsers
-                            name={item.name}
-                            email={item.email}
-                            address={item.address}
-                        />
-                    )}
-                    </tbody>
 
-                </table>
+export default function UsersTable(props) {
+    return (
+        <div className="field-users-table">
+            <div className="field-users-table-head">
+                <p>Users</p>
             </div>
-        )
-    }
+            <table width="100%">
+                <thead>
+                <tr>
+                    <th>Users</th>
+                    <th>Email</th>
+                    <th>Address</th>
+                    <th>Delete</th>
+                    <th>Edit</th>
+                </tr>
+                </thead>
+                <tbody>
+                {props.users.map((item) =>
+                    <ShowUsers
+                        name={item.name}
+                        email={item.email}
+                        address={item.address}
+                    />
+                )}
+                </tbody>
+
+            </table>
+        </div>
+    )
 }
-console.log(22232);
-export default UsersTable;
+// class UsersTable extends React.Component {
+//     render(){
+//         return (
+//             <div className="field-users-table">
+//                 <div className="field-users-table-head">
+//                     <p>Users</p>
+//                 </div>
+//                 <table width="100%">
+//                     <thead>
+//                         <tr>
+//                             <th>Users</th>
+//                             <th>Email</th>
+//                             <th>Address</th>
+//                             <th>Delete</th>
+//                             <th>Edit</th>
+//                         </tr>
+//                     </thead>
+//                     <tbody>
+//                     {this.props.users.map((item) =>
+//                         <ShowUsers
+//                             name={item.name}
+//                             email={item.email}
+//                             address={item.address}
+//                         />
+//                     )}
+//                     </tbody>
+//
+//                 </table>
+//             </div>
+//         )
+//     }
+// }
+// export default UsersTable;
